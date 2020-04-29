5f67370f28adfed6dee644424cd41676ffcd0a40
wangzheng <zheng.wang@fih-foxconn.com>
2020-04-29 17:36:28 +0800

    add navs

--- src/other-notes/vuepress/demo/docs/.vuepress/config.js
+++ src/other-notes/vuepress/demo/docs/.vuepress/config.js
@@ -1,3 +1,28 @@
+const nav = [
+  {
+    text: '首页',
+    link: '/',
+  },
+  {
+    text: '笔记',
+    ariaLabel: '笔记菜单',
+    items: [
+      { text: '前端', link: '/note/front/' },
+      { text: 'php', link: '/note/php/' },
+    ],
+  },
+  {
+    text: '关于',
+    link: '/about/',
+  },
+  {
+    text: 'Github',
+    link: 'https://github.com/Wayley',
+    target: '_blank', // _self
+    rel: '',
+  },
+];
+
 module.exports = {
   title: 'HW',
   description: 'Welcome to the World!',
@@ -8,4 +33,8 @@ module.exports = {
     ],
   ],
   port: 9966, // dev server端口
+  themeConfig: {
+    logo: '', // 导航栏logo
+    nav,
+  },
 };

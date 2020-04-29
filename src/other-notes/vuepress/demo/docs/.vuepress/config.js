d4dd046e39e1fc30bb05e53ad8e7a8e0873aba04
wangzheng <zheng.wang@fih-foxconn.com>
2020-04-29 17:14:19 +0800

    add head config in the config.js

--- src/other-notes/vuepress/demo/docs/.vuepress/config.js
+++ src/other-notes/vuepress/demo/docs/.vuepress/config.js
@@ -1,4 +1,10 @@
 module.exports = {
   title: 'HW',
   description: 'Welcome to the World!',
+  head: [
+    [
+      'link',
+      { rel: 'shortcut icon', href: 'favicon.ico', type: 'image/x-icon' },
+    ],
+  ],
 };

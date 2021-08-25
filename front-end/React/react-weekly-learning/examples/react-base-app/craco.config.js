8fbe09541a513c99fa9b1afb78e2c1a6b49967bf
wangzheng <zheng.wang@fih-foxconn.com>
2021-08-25 15:26:08 +0800

    add craco configurations

---
+++ front-end/React/react-weekly-learning/examples/react-base-app/craco.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: wzheng(hb_wangzheng@163.com)
+ * @Github: https://github.com/wayley
+ * @Company: FOXCONN(-ACKN)
+ * @Copyright: @Wing
+ * @Date: 2021-08-25 15:12:46
+ * @LastEditors: wzheng(hb_wangzheng@163.com)
+ * @LastEditTime: 2021-08-25 15:14:56
+ * @Description:
+ */
+module.exports = {};
